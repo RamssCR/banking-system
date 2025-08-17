ca22812c96714dd27b8644754a0d2de2aab7e763
RamssCR <ramsesgpm@gmail.com>
2025-08-16 19:51:52 -0500

    fix: removed CRLF

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('query parser', 'extended');
   await app.listen(process.env.PORT ?? 3000);
 }
 
