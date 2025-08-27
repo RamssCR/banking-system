f5881b2ed74cd3925982cb5a21694d83d9c6844a
RamssCR <ramsesgpm@gmail.com>
2025-08-27 18:41:22 -0500

    feat: added user module to the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { UsersModule } from './users/users.module.js';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [UsersModule],
 })
 export class AppModule {}
