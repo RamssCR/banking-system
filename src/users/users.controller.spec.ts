976ba695b988c720b7ba24413a765c59dfa93402
RamssCR <ramsesgpm@gmail.com>
2025-08-27 18:43:40 -0500

    feat: created resource for users

---
+++ src/users/users.controller.spec.ts
@@ -0,0 +1,21 @@
+import { beforeEach, describe, expect, test } from 'vitest';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller.js';
+import { UsersService } from './users.service.js';
+
+describe('UsersController', () => {
+  let controller: UsersController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [UsersService],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  test('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
