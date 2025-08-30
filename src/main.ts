59a31e98cfe65c2591e241b2cd28088c43181c0a
RamssCR <ramsesgpm@gmail.com>
2025-08-30 12:41:42 -0500

    feat: added helmet

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 
 const bootstrap = async () => {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const configService = app.get(ConfigService);
+
   app.set('query parser', 'extended');
-  await app.listen(configService.get('PORT') ?? 3000);
+  app.use(helmet());
+
+  const config = app.get(ConfigService);
+  await app.listen(config.get('PORT') ?? 3000);
   console.log(`App running on port ${await app.getUrl()}`);
 };
 
