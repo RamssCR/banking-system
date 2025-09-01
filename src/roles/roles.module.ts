370a59cf0b1a2dda64ee507e6cfd3b589466d71a
RamssCR <ramsesgpm@gmail.com>
2025-08-31 20:29:09 -0500

    feat: added role as importable module

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -8,5 +8,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [TypeOrmModule.forFeature([Role])],
   controllers: [RolesController],
   providers: [RolesService],
+  exports: [RolesService, TypeOrmModule.forFeature([Role])],
 })
 export class RolesModule {}
