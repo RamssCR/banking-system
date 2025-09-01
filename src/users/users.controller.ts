71b163492611078d79c36b1b626cccf07d47e1fd
RamssCR <ramsesgpm@gmail.com>
2025-08-31 20:37:40 -0500

    feat: created all routes for users (super roles only)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PaginationDto } from '#common/dtos/pagination.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  async findAll(@Query() { limit, page }: PaginationDto) {
+    return this.usersService.findAll(page, limit);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number): Promise<User | null> {
+    return this.usersService.findOne(id);
+  }
+
+  @Get()
+  async findOneByEmail(@Query('email') email: string): Promise<User | null> {
+    return this.usersService.findOneByEmail(email);
+  }
+
+  @Post()
+  async create(@Body() user: CreateUserDto): Promise<User> {
+    return this.usersService.create(user);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: number, @Body() user: UpdateUserDto) {
+    return this.usersService.update(id, user);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number) {
+    return this.usersService.remove(id);
+  }
 }
