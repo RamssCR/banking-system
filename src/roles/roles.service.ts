d2efc3222831a9de3bec3ecf2533ddd6b736a276
RamssCR <ramsesgpm@gmail.com>
2025-08-31 20:35:51 -0500

    feat: added error handling to all methods

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,7 +1,14 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { QueryFailedError, Repository } from 'typeorm';
+import { CreateRoleDto } from './dto/create-role.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { Role } from './entities/role.entity';
+import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Injectable()
 export class RolesService {
@@ -15,23 +22,68 @@ export class RolesService {
   }
 
   async findOne(id: number): Promise<Role | null> {
-    const role = await this.roleRepository.findOneBy({ id });
-    if (!role) throw new BadRequestException(`Role ${id} not found`);
-    return role;
+    try {
+      const role = await this.roleRepository.findOneByOrFail({ id });
+      if (!role) throw new BadRequestException(`Role ${id} not found`);
+
+      return role;
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        throw new InternalServerErrorException(error.message);
+      }
+
+      throw new InternalServerErrorException(
+        'An error has ocurred while getting the role',
+      );
+    }
   }
 
-  async create(role: Role): Promise<Role> {
-    const createdRole = this.roleRepository.create(role);
-    return await this.roleRepository.save(createdRole);
+  async create(role: CreateRoleDto): Promise<Role> {
+    try {
+      const createdRole = this.roleRepository.create(role);
+      return await this.roleRepository.save(createdRole);
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        throw new InternalServerErrorException(error.message);
+      }
+
+      throw new InternalServerErrorException(
+        'An error has ocurred while creating the role',
+      );
+    }
   }
 
-  async update(id: number, role: Partial<Role>) {
-    await this.roleRepository.update(id, role);
-    return this.roleRepository.findOneBy({ id });
+  async update(id: number, dto: UpdateRoleDto): Promise<Role | null> {
+    try {
+      const role = await this.findOne(id);
+      if (!role) throw new NotFoundException(`Role with ID ${id} not found`);
+
+      await this.roleRepository.update(id, dto);
+      return this.findOne(id);
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        throw new InternalServerErrorException(error.message);
+      }
+
+      throw new InternalServerErrorException(
+        'An error has ocurred while updating the role',
+      );
+    }
   }
 
   async remove(id: number): Promise<void> {
-    await this.roleRepository.findOneBy({ id });
-    await this.roleRepository.delete(id);
+    try {
+      const result = await this.roleRepository.softDelete(id);
+      if (result.affected === 0)
+        throw new NotFoundException(`User with ID ${id} not found`);
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        throw new InternalServerErrorException(error.message);
+      }
+
+      throw new InternalServerErrorException(
+        'An error has ocurred while deleting the role',
+      );
+    }
   }
 }
