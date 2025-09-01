0ba5e72881b4b657ca2eae8a5640783b4cdf5347
RamssCR <ramsesgpm@gmail.com>
2025-08-31 20:34:37 -0500

    feat: added hook to hash password

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,6 +1,8 @@
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne } from 'typeorm';
 import { BaseEntity } from '#common/entities/base.entity';
+import { Exclude } from 'class-transformer';
 import { Role } from '#roles/entities/role.entity';
+import bcrypt from 'bcryptjs';
 
 @Entity()
 export class User extends BaseEntity {
@@ -10,9 +12,19 @@ export class User extends BaseEntity {
   @Column({ type: 'varchar', length: 50, unique: true })
   email: string;
 
+  @Exclude()
   @Column({ type: 'varchar', length: 300 })
   password: string;
 
   @ManyToOne(() => Role, (role) => role.users)
   role: Role;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async hashPassword() {
+    if (this.password) {
+      const salt = await bcrypt.genSalt();
+      this.password = await bcrypt.hash(this.password, salt);
+    }
+  }
 }
