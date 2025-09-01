4d3bbebe94c2fb000ebb8be81120a6b7072521d0
RamssCR <ramsesgpm@gmail.com>
2025-08-31 20:46:35 -0500

    feat: added global interceptor module handler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { InterceptorsModule } from './common/interceptors/interceptors.module.js';
 import { Module } from '@nestjs/common';
 import { RolesModule } from './roles/roles.module';
 import { UsersModule } from './users/users.module';
@@ -8,6 +9,6 @@ import { UsersModule } from './users/users.module';
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UsersModule, RolesModule, DatabaseModule],
+  imports: [UsersModule, RolesModule, DatabaseModule, InterceptorsModule],
 })
 export class AppModule {}
