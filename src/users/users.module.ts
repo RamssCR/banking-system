54fd5953dcd97bf898802e43565f7af85c688594
RamssCR <ramsesgpm@gmail.com>
2025-09-03 22:16:20 -0500

    feat: made user module exportable for DI

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -9,5 +9,6 @@ import { UsersService } from './users.service';
   imports: [TypeOrmModule.forFeature([User]), RolesModule],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService, TypeOrmModule.forFeature([User])],
 })
 export class UsersModule {}
