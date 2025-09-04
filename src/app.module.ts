d9988b35cee8490dd5c3df6d61f4d0ffd963fcac
RamssCR <ramsesgpm@gmail.com>
2025-09-03 19:45:27 -0500

    feat: created authentication module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,17 @@ import { InterceptorsModule } from './common/interceptors/interceptors.module.js
 import { Module } from '@nestjs/common';
 import { RolesModule } from './roles/roles.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UsersModule, RolesModule, DatabaseModule, InterceptorsModule],
+  imports: [
+    AuthModule,
+    DatabaseModule,
+    InterceptorsModule,
+    RolesModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
---
+++ src/auth/auth.controller.spec.ts
@@ -0,0 +1,21 @@
+import { beforeEach, describe, expect, test } from 'vitest';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+describe('AuthController', () => {
+  let controller: AuthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AuthController],
+      providers: [AuthService],
+    }).compile();
+
+    controller = module.get<AuthController>(AuthController);
+  });
+
+  test('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
