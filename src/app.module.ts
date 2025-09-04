f39e78a2b3128b91662ea9653ac5b5684d7a451b
RamssCR <ramsesgpm@gmail.com>
2025-09-03 22:15:45 -0500

    feat: added global pipe validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { InterceptorsModule } from './common/interceptors/interceptors.module.js';
+import { InterceptorsModule } from './common/interceptors/interceptors.module';
 import { Module } from '@nestjs/common';
 import { RolesModule } from './roles/roles.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PipesModule } from '#common/pipes/pipes.module';
 
 @Module({
   controllers: [AppController],
@@ -14,6 +15,7 @@ import { AuthModule } from './auth/auth.module';
     AuthModule,
     DatabaseModule,
     InterceptorsModule,
+    PipesModule,
     RolesModule,
     UsersModule,
   ],
