4d2ade249acf3d4e431dd78c1cb0e3cd9ddaa45c
RamssCR <ramsesgpm@gmail.com>
2025-09-07 17:05:59 -0500

    chore: ordered imports alphabetically

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { InterceptorsModule } from './common/interceptors/interceptors.module';
 import { Module } from '@nestjs/common';
+import { PipesModule } from '#common/pipes/pipes.module';
 import { RolesModule } from './roles/roles.module';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { PipesModule } from '#common/pipes/pipes.module';
 
 @Module({
   controllers: [AppController],
