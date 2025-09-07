9510bf2e82bb599827bbe2609388658580750b77
RamssCR <ramsesgpm@gmail.com>
2025-09-07 17:12:15 -0500

    feat: added auth token modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { AccessModule } from '#auth/access.module';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { Module } from '@nestjs/common';
+import { RefreshModule } from '#auth/refresh.module';
 import { UsersModule } from '#users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [AccessModule, RefreshModule, UsersModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
