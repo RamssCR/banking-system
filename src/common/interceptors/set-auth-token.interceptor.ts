46d7d84c494b7e3e5b893df5ad27b73443d2ade1
RamssCR <ramsesgpm@gmail.com>
2025-09-07 17:11:35 -0500

    feat: created interceptor for setting tokens on cookies

---
+++ src/common/interceptors/set-auth-token.interceptor.ts
@@ -0,0 +1,44 @@
+import {
+  type ExecutionContext,
+  type NestInterceptor,
+  CallHandler,
+  Injectable,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+import type { AuthResponse } from '#auth/interfaces/auth-response.interface';
+import type { Response } from 'express';
+
+@Injectable()
+export class SetAuthTokenInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
+    const ctx = context.switchToHttp();
+    const res = ctx.getResponse<Response>();
+
+    return next.handle().pipe(
+      map((data: AuthResponse) => {
+        if (data.access_token) {
+          res.cookie('access_token', data.access_token, {
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge: 1000 * 60 * 60 * 24,
+            secure: false,
+          });
+        }
+
+        if (data.refresh_token) {
+          res.cookie('refresh_token', data.refresh_token, {
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+            secure: false,
+          });
+        }
+
+        return {
+          message: 'login successful',
+          user: data.user,
+        };
+      }),
+    );
+  }
+}
