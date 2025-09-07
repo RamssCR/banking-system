0dba38822a7fb773ad50934ccabe068aed6fb1a1
RamssCR <ramsesgpm@gmail.com>
2025-09-07 17:10:08 -0500

    feat: added public controller endpoints for login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AuthResponse } from './interfaces/auth-response.interface';
 import { AuthService } from './auth.service';
+import { SetAuthTokenInterceptor } from '#common/interceptors/set-auth-token.interceptor';
+import { SignInDto } from './dto/sign-in.dto';
+import { SignUpDto } from './dto/sign-up.dto';
 
 @Controller('auth')
+@UseInterceptors(SetAuthTokenInterceptor)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(200)
+  @Post('signin')
+  async signIn(@Body() dto: SignInDto): Promise<AuthResponse> {
+    return this.authService.signIn(dto);
+  }
+
+  @Post('signup')
+  async signUp(@Body() dto: SignUpDto): Promise<AuthResponse> {
+    return this.authService.signUp(dto);
+  }
 }
