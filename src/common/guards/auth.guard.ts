0215fadb6db1a102c33f47dc5c411c6a5b5a001a
RamssCR <ramsesgpm@gmail.com>
2025-09-11 16:38:29 -0500

    feat: added public validation to the authentication guard

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -6,17 +6,26 @@ import {
 } from '@nestjs/common';
 import { AuthRequest } from '#types/auth';
 import { ConfigService } from '@nestjs/config';
-import { JwtPayload } from '#auth/interfaces/jwt-payload.interface.js';
+import { IS_PUBLIC_KEY } from '#common/decorators/public.decorator';
+import { JwtPayload } from '#auth/interfaces/jwt-payload.interface';
 import { JwtService } from '@nestjs/jwt';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private readonly jwtService: JwtService,
     private readonly config: ConfigService,
+    private readonly reflector: Reflector,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) return true;
+
     const request = context.switchToHttp().getRequest<AuthRequest>();
     const token = request.cookies?.['access_token'];
 
