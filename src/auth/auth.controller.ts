040d34d25e94719db35e0c05463f4d6d5e745195
RamssCR <ramsesgpm@gmail.com>
2025-09-11 16:36:32 -0500

    feat: added public decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,13 +3,12 @@ import {
   Controller,
   HttpCode,
   Post,
-  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { AuthGuard } from '#common/guards/auth.guard';
 import { AuthResponse } from './interfaces/auth-response.interface';
 import { AuthService } from './auth.service';
 import { Cookies } from '#common/decorators/cookies.decorator';
+import { Public } from '#common/decorators/public.decorator';
 import { SetAuthTokenInterceptor } from '#common/interceptors/set-auth-token.interceptor';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
@@ -19,22 +18,28 @@ import { SignUpDto } from './dto/sign-up.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @HttpCode(200)
   @Post('signin')
   async signIn(@Body() dto: SignInDto): Promise<AuthResponse> {
     return this.authService.signIn(dto);
   }
 
+  @Public()
   @Post('signup')
   async signUp(@Body() dto: SignUpDto): Promise<AuthResponse> {
     return this.authService.signUp(dto);
   }
 
+  @Public()
   @Post('refresh')
-  @UseGuards(AuthGuard)
   async refresh(
     @Cookies('refresh_token') token: string,
   ): Promise<Pick<AuthResponse, 'access_token'>> {
     return await this.authService.refresh(token);
   }
+
+  @HttpCode(200)
+  @Post('signout')
+  async signout(): Promise<void> {}
 }
