21120d30ff3dadab4e7c3396b36e783d6927f986
RamssCR <ramsesgpm@gmail.com>
2025-09-12 21:15:27 -0500

    feat: added role validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,6 +10,7 @@ import {
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PaginationDto } from '#common/dtos/pagination.dto';
+import { Roles } from '#common/decorators/roles.decorator';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
@@ -18,6 +19,7 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Roles('admin', 'moderator')
   @Get()
   async findAll(@Query() { limit, page }: PaginationDto) {
     return this.usersService.findAll(page, limit);
@@ -28,16 +30,19 @@ export class UsersController {
     return this.usersService.findOne(id);
   }
 
+  @Roles('admin')
   @Post()
   async create(@Body() user: CreateUserDto): Promise<User> {
     return this.usersService.create(user);
   }
 
+  @Roles('admin')
   @Patch(':id/restore')
   async restore(@Param('id') id: number): Promise<void> {
     return this.usersService.restore(id);
   }
 
+  @Roles('admin')
   @Patch(':id')
   async update(
     @Param('id') id: number,
@@ -46,8 +51,9 @@ export class UsersController {
     return this.usersService.update(id, user);
   }
 
+  @Roles('admin')
   @Delete(':id')
-  async remove(@Param('id') id: number) {
+  async remove(@Param('id') id: number): Promise<void> {
     return this.usersService.remove(id);
   }
 }
