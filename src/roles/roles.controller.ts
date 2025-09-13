33ff045166e7a3ea2c254cea707520771516f1c7
RamssCR <ramsesgpm@gmail.com>
2025-09-12 21:19:22 -0500

    feat: added roles permissions to the role endpoint

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -9,6 +9,7 @@ import {
 } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { Role } from './entities/role.entity';
+import { Roles } from '#common/decorators/roles.decorator';
 import { RolesService } from './roles.service';
 import { UpdateRoleDto } from './dto/update-role.dto';
 
@@ -16,26 +17,31 @@ import { UpdateRoleDto } from './dto/update-role.dto';
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
+  @Roles('admin', 'moderator')
   @Get()
   async findAll(): Promise<Role[]> {
     return await this.rolesService.findAll();
   }
 
+  @Roles('admin', 'moderator')
   @Get(':id')
   async findOne(@Param('id') id: number): Promise<Role> {
     return await this.rolesService.findOne(id);
   }
 
+  @Roles('admin')
   @Post()
   async create(@Body() role: CreateRoleDto): Promise<Role> {
     return await this.rolesService.create(role);
   }
 
+  @Roles('admin')
   @Patch(':id/restore')
   async restore(@Param('id') id: number): Promise<void> {
     return await this.rolesService.restore(id);
   }
 
+  @Roles('admin')
   @Patch(':id')
   async update(
     @Param('id') id: number,
@@ -44,6 +50,7 @@ export class RolesController {
     return await this.rolesService.update(id, role);
   }
 
+  @Roles('admin')
   @Delete(':id')
   async remove(@Param('id') id: number): Promise<void> {
     return await this.rolesService.remove(id);
