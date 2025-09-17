44ed8924d1748a5e3a74e88599281bf45215be35
RamssCR <ramsesgpm@gmail.com>
2025-09-16 21:52:05 -0500

    feat: created algorithm for account number creation

--- src/common/helpers/constants.ts
+++ src/common/helpers/constants.ts
@@ -13,4 +13,22 @@ const MAX_AGE_SEVEN_DAYS = MAX_AGE_ONE_DAY * 7;
  */
 const REFRESH_TOKEN_SALT_ROUNDS = 12;
 
-export { MAX_AGE_ONE_DAY, MAX_AGE_SEVEN_DAYS, REFRESH_TOKEN_SALT_ROUNDS };
+/**
+ * Prefix for account numbers.
+ * This is used to identify account numbers in the system.
+ */
+const ACCOUNT_NUMBER_PREFIX = '01';
+
+/**
+ * Base value for generating account numbers.
+ * This is used as a starting point for account number generation.
+ */
+const ACCOUNT_NUMBER_BASE_VALUE = 10000000;
+
+export {
+  MAX_AGE_ONE_DAY,
+  MAX_AGE_SEVEN_DAYS,
+  REFRESH_TOKEN_SALT_ROUNDS,
+  ACCOUNT_NUMBER_PREFIX,
+  ACCOUNT_NUMBER_BASE_VALUE,
+};
