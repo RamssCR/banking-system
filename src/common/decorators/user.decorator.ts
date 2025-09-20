1d2144e41054f45f00cef8bf4db239684b23651c
RamssCR <ramsesgpm@gmail.com>
2025-09-20 12:35:23 -0500

    fix: added fallback if payload doesn't exist

--- src/common/decorators/user.decorator.ts
+++ src/common/decorators/user.decorator.ts
@@ -1,8 +1,9 @@
 import { AuthRequest } from '#types/auth';
-import { JwtPayload } from '#auth/interfaces/jwt-payload.interface.js';
-import { createParamDecorator } from '@nestjs/common';
+import { JwtPayload } from '#auth/interfaces/jwt-payload.interface';
+import { createParamDecorator, UnauthorizedException } from '@nestjs/common';
 
 export const User = createParamDecorator((user: keyof JwtPayload, context) => {
   const request = context.switchToHttp().getRequest<AuthRequest>();
   if (request.user) return request.user[user];
+  throw new UnauthorizedException('User not authenticated');
 });
